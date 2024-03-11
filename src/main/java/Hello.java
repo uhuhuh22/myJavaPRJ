dcfce342ee45454242f91afb99886f0cfc8aa3d1
uhuhuh22 <8908alex@gmail.com>
2024-03-11 15:21:05 +0900

    소스 수정

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,8 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello!! My First Java Coding!!");
 
-        System.out.println("Edit Java code!!");
-
         System.out.println("Hello!! My Twice Java Coding!!");
     }
 }
