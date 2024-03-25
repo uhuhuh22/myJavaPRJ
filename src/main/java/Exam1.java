b59b150f313692cacf8cd454bae4d49a634b40d9
uhuhuh22 <8908alex@gmail.com>
2024-03-25 17:47:58 +0900

    소스 수정

--- src/main/java/Exam1.java
+++ src/main/java/Exam1.java
@@ -3,9 +3,12 @@ public class Exam1 {
         String log = "127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326 \"http://www.example.com/start.html\" \"Mozilla/4.08 [en] (Win98; I ;Nav)\"";
 
         System.out.println(log.indexOf(" "));
+
         System.out.println(log.substring(0,(log.indexOf(" "))));
-        System.out.println(log.indexOf("f"));
-        System.out.println(log.substring(12,17));
+
+        System.out.println(log.substring((log.indexOf("\"")+1),(log.indexOf(" /"))));
+
+
 
 
     }
