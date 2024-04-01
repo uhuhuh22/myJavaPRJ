8e60d16c8224881e8d293352bb8ca5a639dec399
uhuhuh22 <8908alex@gmail.com>
2024-04-01 13:37:47 +0900

    소스 수정

---
+++ src/main/java/Code5_2.java
@@ -0,0 +1,10 @@
+public class Code5_2 {
+    public static void main(String[] args) {
+        int num = 99;
+
+        if(num < 100) {
+            System.out.print("100보다 ");
+            System.out.print("작습니다.");
+        }
+    }
+}
