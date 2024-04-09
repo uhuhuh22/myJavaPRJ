1614bd86aaa882085d42ddf41cda3629bfc29146
uhuhuh22 <8908alex@gmail.com>
2024-04-09 14:38:17 +0900

    소스 수정

---
+++ src/main/java/quiz1_2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class quiz1_2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("첫 번째 값을 입력하세요 :");
+        int a = s.nextInt();
+
+        System.out.print("두 번째 값을 입력하세요 :");
+        int b = s.nextInt();
+
+        for (int i = a; i < b +1 ; i++) {
+        System.out.print(i + " ");}
+    }
+}
