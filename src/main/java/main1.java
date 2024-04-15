3ab3f6199e3003ac49de27cc44f1fa51cbc328d8
uhuhuh22 <8908alex@gmail.com>
2024-04-15 13:43:16 +0900

    h

---
+++ src/main/java/main1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class main1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(in);
+
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+            int hap = 0;
+            if (a < b) {
+                for (int i = a; i <= b; i++) {
+
+                    if (i % 2 != 0) {
+                        hap += i;
+                    }
+                }
+            } else if (a > b) {
+
+                for (int i = a; i >= b; i--) {
+                    if (i % 2 != 0){
+                        hap += i;
+                    }
+                }
+
+            }
+        System.out.println(hap);
+        }
+    }
+
+
+
+
